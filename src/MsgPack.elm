efffbd50f947be2ac187146ea4ebd89d1779bf74
Oliver Sõro <oliver.s6ro@gmail.com>
2017-11-17 22:54:35 +0200

    Start decoding functionality for parsing into MsgPack: array, map, nil, bool, bin.

--- src/MsgPack.elm
+++ src/MsgPack.elm
@@ -1,31 +1,71 @@
 module MsgPack
     exposing
-        ( MsgPack
+        ( BlockBytes(..)
+        , Error(..)
+        , MsgPack
+        , append
+        , parseArray
+        , parseBin
+        , parseBool
+        , parseMap
+        , parseNil
         )
 
 {-| Types for MessagePack specification.
 
-@docs MsgPack
+
+# Specification type
+
+@docs MsgPack, Error
+
+
+# Decoding helpers
+
+@docs BlockBytes
+
+
+## Collections
+
+@docs parseArray, parseMap, append
+
+
+## Types
+
+@docs parseNil, parseBool, parseBin
 
 -}
 
+import Bitwise
 import Result exposing (Result)
 
 
+{-| Elm's MessagePack byte-stream wrapper.
+-}
 type MsgPack
     = Nil Int
-    | False_ Int
-    | True_ Int
-    | Array_ Int Container
+    | Array_ Int ArrayRecord
     | Bin Int BinRecord
+    | Bool_ Int Bool
     | Ext Int ExtRecord
     | Float_ Int ValueRecord
     | Integer Int ValueRecord
-    | Map Int Container
+    | Map Int MapRecord
     | Str Int ValueRecord
     | UInt Int ValueRecord
 
 
+{-| MessagePack parsing error.
+-}
+type Error
+    = AppendFailure String
+    | EmptyStream
+    | IncorrectStart String
+    | IncorrectBlockSize String
+    | NotImplemented
+
+
+{-| `MsgPack` type size indicator for parse helpers
+-}
 type BlockBytes
     = B0
     | B1
@@ -33,20 +73,20 @@ type BlockBytes
     | B4
     | B8
     | B16
-    | B32
-    | B64
+
+
+type alias ArrayRecord =
+    { block : BlockBytes
+    , total : Int
+    , count : Int
+    , objects : List MsgPack
+    }
 
 
 type alias BinRecord =
     { block : BlockBytes
-    , bytes : List Int
     , bsize : Int
-    }
-
-
-type alias Container =
-    { block : BlockBytes
-    , objects : List MsgPack
+    , bytes : List Int
     }
 
 
@@ -59,6 +99,14 @@ type alias ExtRecord =
     }
 
 
+type alias MapRecord =
+    { block : BlockBytes
+    , total : Int
+    , count : Int
+    , objects : List ( MsgPack, MsgPack )
+    }
+
+
 type alias ValueRecord =
     { block : BlockBytes
     , signed : Bool
@@ -66,163 +114,298 @@ type alias ValueRecord =
     }
 
 
-toByte : MsgPack -> Result String Int
-toByte msgpack =
-    case msgpack of
-        Nil _ ->
-            Ok 0xC0
 
-        False_ _ ->
-            Ok 0xC2
+-- Parsing Helpers
 
-        True_ _ ->
-            Ok 0xC3
 
-        Array_ _ { block } ->
-            case block of
-                B0 ->
-                    -- fix: 1001xxxx 0x90 - 0x9f
-                    Ok 0x90
+{-| @private
+-}
+blockBytesCount : BlockBytes -> Int
+blockBytesCount block =
+    case block of
+        B0 ->
+            0
 
-                B16 ->
-                    Ok 0xDC
+        B1 ->
+            1
 
-                B32 ->
-                    Ok 0xDD
+        B2 ->
+            2
 
-                _ ->
-                    Err "Invalid Array_ BlockBytes value. Allowed values: B0, B16, B32."
+        B4 ->
+            4
 
-        Bin _ { block } ->
-            case block of
-                B8 ->
-                    Ok 0xC4
+        B8 ->
+            8
 
-                B16 ->
-                    Ok 0xC5
+        B16 ->
+            16
 
-                B32 ->
-                    Ok 0xC6
 
-                _ ->
-                    Err "Invalid Bin BlockBytes value. Allowed values: B8, B16 or B32."
+{-| @private
+-}
+dataCount : Int -> BlockBytes -> List Int -> Int
+dataCount byte block stream =
+    if block == B0 then
+        let
+            leftNibble =
+                Bitwise.shiftRightBy 4 byte
+        in
+        Bitwise.xor byte leftNibble
+    else if block == B1 then
+        byte
+    else
+        stream
+            |> List.take (blockBytesCount block)
+            |> List.foldl (\b a -> Bitwise.shiftLeftBy 8 b) 0
 
-        Ext _ { block, fixed } ->
-            if fixed then
-                case block of
-                    B1 ->
-                        Ok 0xD4
 
-                    B2 ->
-                        Ok 0xD5
+{-| Parse a MessagePack nil from byte stream.
+-}
+parseNil : Int -> Result Error MsgPack
+parseNil byte =
+    Nil byte |> Ok
 
-                    B4 ->
-                        Ok 0xD6
 
-                    B8 ->
-                        Ok 0xD7
+{-| Parse a MessagePack bool from byte stream.
+-}
+parseBool : Int -> Bool -> Result Error MsgPack
+parseBool byte flag =
+    Bool_ byte flag |> Ok
 
-                    B16 ->
-                        Ok 0xD8
 
-                    _ ->
-                        Err "Invalid fixed Ext BlockBytes values. Allowed values: B1, B2, B4, B8, B16."
+{-| Parse a MessagePack bin from byte stream.
+-}
+parseBin : Int -> BlockBytes -> List Int -> ( Result Error MsgPack, List Int )
+parseBin byte block stream =
+    let
+        validBlocks =
+            [ B1, B2, B4 ]
+
+        byteCount =
+            dataCount byte block stream
+    in
+    if List.member block validBlocks then
+        ( Bin
+            byte
+            { block = block
+            , bsize = byteCount
+            , bytes = List.take byteCount stream
+            }
+            |> Ok
+        , List.drop byteCount stream
+        )
+    else
+        ( ("Unsuppored bin type (size): " ++ toString block)
+            |> IncorrectBlockSize
+            |> Err
+        , stream
+        )
+
+
+{-| Parse a MessagePack array from byte stream.
+
+Supported MessagePack types: fixarray, array 16, array 32.
+
+-}
+parseArray : Int -> BlockBytes -> List Int -> Result Error MsgPack
+parseArray byte block stream =
+    let
+        validBlocks =
+            [ B0, B2, B4 ]
+    in
+    if List.member block validBlocks then
+        Array_
+            byte
+            { block = block
+            , total = dataCount byte block stream
+            , count = 0
+            , objects = []
+            }
+            |> Ok
+    else
+        ("Unsupported array type (size): " ++ toString block)
+            |> IncorrectBlockSize
+            |> Err
+
+
+{-| Parse a MessagePack map from byte stream.
+
+Supported MessagePack types: fixmap, map 16, map 32.
+
+-}
+parseMap : Int -> BlockBytes -> List Int -> Result Error MsgPack
+parseMap byte block stream =
+    let
+        validBlocks =
+            [ B0, B2, B4 ]
+    in
+    if List.member block validBlocks then
+        Map
+            byte
+            { block = block
+            , total = 2 * dataCount byte block stream
+            , count = 0
+            , objects = []
+            }
+            |> Ok
+    else
+        ("Unsupported map type (size): " ++ toString block)
+            |> IncorrectBlockSize
+            |> Err
+
+
+{-| Append a `MsgPack` item to `MsgPack` collection (array or map).
+
+All collection items must have the same type. First item added to the collection
+sets the type of the collection.
+
+-}
+append : Maybe MsgPack -> Result Error MsgPack -> Result Error MsgPack
+append collection item =
+    let
+        updateArray b r i =
+            Array_
+                b
+                { r
+                    | count = r.count + 1
+                    , objects =
+                        if r.total > r.count + 1 then
+                            i :: r.objects
+                        else
+                            i :: r.objects |> List.reverse
+                }
+
+        updateMapKey b r k =
+            Map b { r | count = r.count + 1, objects = ( k, k ) :: r.objects }
+
+        updateMapValue b r v =
+            Map
+                b
+                { r
+                    | count = r.count + 1
+                    , objects =
+                        if r.total > r.count + 1 then
+                            case r.objects of
+                                [] ->
+                                    r.objects
+
+                                ( k, _ ) :: rest ->
+                                    ( k, v ) :: rest
+                        else
+                            case r.objects of
+                                [] ->
+                                    r.objects
+
+                                ( k, _ ) :: rest ->
+                                    ( k, v ) :: rest |> List.reverse
+                }
+    in
+    case collection of
+        Just (Array_ b r) ->
+            if r.count < r.total then
+                case
+                    ( List.head r.objects
+                    , item
+                    )
+                of
+                    ( Nothing, Ok i ) ->
+                        updateArray b r i |> Ok
+
+                    ( Just headItem, Ok i ) ->
+                        if sameType headItem i then
+                            updateArray b r i |> Ok
+                        else
+                            "Array item type mismatch."
+                                |> AppendFailure
+                                |> Err
+
+                    ( Nothing, Err error ) ->
+                        Err error
+
+                    ( Just _, Err error ) ->
+                        Err error
             else
-                case block of
-                    B8 ->
-                        Ok 0xC7
+                "Array full, expected size of items exceeded."
+                    |> AppendFailure
+                    |> Err
 
-                    B16 ->
-                        Ok 0xC8
+        Just (Map b r) ->
+            if r.count < r.total then
+                case
+                    ( List.head r.objects
+                    , item
+                    )
+                of
+                    ( Nothing, Ok i ) ->
+                        updateMapKey b r i |> Ok
 
-                    B32 ->
-                        Ok 0xC9
+                    ( Just ( k, v ), Ok i ) ->
+                        if (r.count + 1) % 2 == 1 then
+                            -- odd, map key
+                            if sameType k i then
+                                updateMapKey b r i |> Ok
+                            else
+                                "Map key type mismatch."
+                                    |> AppendFailure
+                                    |> Err
+                        else if sameType v i then
+                            updateMapValue b r i |> Ok
+                        else
+                            "Map value type mismatch."
+                                |> AppendFailure
+                                |> Err
 
-                    _ ->
-                        Err "Invalid variable Ext BlockBytes value. Allowed values: B8, B16, B16."
+                    ( Nothing, Err error ) ->
+                        Err error
 
-        Float_ _ { block } ->
-            case block of
-                B32 ->
-                    Ok 0xCA
+                    ( Just _, Err error ) ->
+                        Err error
+            else
+                "Map full, expected size of items exceeded."
+                    |> AppendFailure
+                    |> Err
 
-                B64 ->
-                    Ok 0xCB
+        _ ->
+            "First argument must be a `MsgPack` collection: array or map."
+                |> AppendFailure
+                |> Err
 
-                _ ->
-                    Err "Invalid Float BlockBytes value. Allowed values: B32, B64."
 
-        Integer _ { block, signed } ->
-            case block of
-                B0 ->
-                    if signed then
-                        -- fix: positive fixint 0xxxxxxx 0x00 - 0x7f
-                        Ok 0x00
-                    else
-                        -- negative fixint 111xxxxx 0xe0 - 0xff
-                        Ok 0xE0
+{-| @private
+-}
+sameType : MsgPack -> MsgPack -> Bool
+sameType a b =
+    case ( a, b ) of
+        ( Nil _, Nil _ ) ->
+            True
 
-                B8 ->
-                    Ok 0xD0
+        ( Array_ _ _, Array_ _ _ ) ->
+            True
 
-                B16 ->
-                    Ok 0xD1
+        ( Bin _ _, Bin _ _ ) ->
+            True
 
-                B32 ->
-                    Ok 0xD2
+        ( Bool_ _ _, Bool_ _ _ ) ->
+            True
 
-                B64 ->
-                    Ok 0xD3
+        ( Ext _ _, Ext _ _ ) ->
+            True
 
-                _ ->
-                    Err "Invalid Integer BlockBytes value. Allowed values: B0, B8, B16, B32, B64"
+        ( Float_ _ _, Float_ _ _ ) ->
+            True
 
-        Map _ { block } ->
-            case block of
-                B0 ->
-                    -- fix: 1000xxxx 0x80 - 0x8f
-                    Ok 0x80
+        ( Integer _ _, Integer _ _ ) ->
+            True
 
-                B16 ->
-                    Ok 0xDE
+        ( Map _ _, Map _ _ ) ->
+            True
 
-                B32 ->
-                    Ok 0xDF
+        ( Str _ _, Str _ _ ) ->
+            True
 
-                _ ->
-                    Err "Invalid Map BlockBytes value. Allowed values: B0, B16, B32."
+        ( UInt _ _, UInt _ _ ) ->
+            True
 
-        Str _ { block, bytes } ->
-            case block of
-                B0 ->
-                    -- fix this, bit mask with size: 0xA0 - 0xBF
-                    Ok 0xA0
-
-                B8 ->
-                    Ok 0xD9
-
-                B16 ->
-                    Ok 0xDA
-
-                B32 ->
-                    Ok 0xDB
-
-                _ ->
-                    Err "Invalid Str BlockBytes value. Allowed values: B0, B8, B16, B32"
-
-        UInt _ { block } ->
-            case block of
-                B8 ->
-                    Ok 0xCC
-
-                B16 ->
-                    Ok 0xCD
-
-                B32 ->
-                    Ok 0xCE
-
-                B64 ->
-                    Ok 0xCF
-
-                _ ->
-                    Err "Invalid Integer BlockBytes value. Allowed values: B8, B16, B32, B64"
+        _ ->
+            False
--- src/MsgPack/Decoding.elm
+++ src/MsgPack/Decoding.elm
@@ -14,7 +14,8 @@ module MsgPack.Decoding
 
 import Bitwise
 import Char
-import MsgPack exposing (MsgPack)
+import MsgPack exposing (BlockBytes(..), Error(..), MsgPack)
+import Result
 
 
 {-| Convert to bytes from ISO-8859-1 string representation.
@@ -27,165 +28,190 @@ toBytes bstr =
 
 {-| Parse a list of bytes into `MsgPack`.
 -}
-toMsgPack : List Int -> Maybe MsgPack
+toMsgPack : List Int -> Result Error MsgPack
 toMsgPack bytes =
     let
         parse blist accum =
             case blist of
                 [] ->
-                    accum
+                    Result.fromMaybe MsgPack.EmptyStream accum
 
                 b :: list ->
-                    parseByte b list accum
-                        |> parse list
+                    case parseByte b list accum of
+                        ( Err error, _ ) ->
+                            Err error
+
+                        ( Ok mp, stream ) ->
+                            parse stream <| Just mp
     in
     parse bytes Nothing
 
 
 {-| @private
 -}
-parseByte : Int -> List Int -> Maybe MsgPack -> Maybe MsgPack
-parseByte b bytes msgpack =
+parseByte : Int -> List Int -> Maybe MsgPack -> ( Result Error MsgPack, List Int )
+parseByte b bytes accum =
     case b of
         0xC0 ->
             -- nil
-            Nothing
+            ( MsgPack.parseNil b
+                |> MsgPack.append accum
+            , bytes
+            )
 
         0xC2 ->
             -- false
-            Nothing
+            ( MsgPack.parseBool b False
+                |> MsgPack.append accum
+            , bytes
+            )
 
         0xC3 ->
             -- true
-            Nothing
+            ( MsgPack.parseBool b True
+                |> MsgPack.append accum
+            , bytes
+            )
 
         0xC4 ->
             -- bin8
-            Nothing
+            MsgPack.parseBin b B1 bytes
 
         0xC5 ->
             -- bin16
-            Nothing
+            MsgPack.parseBin b B2 bytes
 
         0xC6 ->
             -- bin32
-            Nothing
+            MsgPack.parseBin b B4 bytes
 
         0xC7 ->
             -- ext8
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xC8 ->
             -- ext16
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xC9 ->
             -- ext32
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xCA ->
             -- float32
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xCB ->
             -- float64
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xCC ->
             -- uint8
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xCD ->
             -- uint16
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xCE ->
             -- uint32
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xCF ->
             -- uint64
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD0 ->
             -- int8
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD1 ->
             -- int16
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD2 ->
             -- int32
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD3 ->
             -- int64
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD4 ->
             -- fixext1
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD5 ->
             -- fixext2
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD6 ->
             -- fixext4
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD7 ->
             -- fixext8
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD8 ->
             -- fixext16
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xD9 ->
             -- str8
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xDA ->
             -- str16
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xDB ->
             -- str32
-            Nothing
+            ( Err MsgPack.NotImplemented, bytes )
 
         0xDC ->
             -- array16
-            Nothing
+            ( MsgPack.parseArray b B2 bytes
+            , bytes
+            )
 
         0xDD ->
             -- array32
-            Nothing
+            ( MsgPack.parseArray b B4 bytes
+            , bytes
+            )
 
         0xDE ->
             -- map16
-            Nothing
+            ( MsgPack.parseMap b B2 bytes
+            , bytes
+            )
 
         0xDF ->
             -- map32
-            Nothing
+            ( MsgPack.parseMap b B4 bytes
+            , bytes
+            )
 
         _ ->
             if b >= 0x00 && b <= 0x7F then
                 -- pos fixint
-                Nothing
+                ( Err MsgPack.NotImplemented, bytes )
             else if b >= 0x80 && b <= 0x8F then
                 -- fixmap
-                Nothing
+                ( MsgPack.parseMap b B0 bytes
+                , bytes
+                )
             else if b >= 0x90 && b <= 0x9F then
                 -- fixarray
-                Nothing
+                ( MsgPack.parseArray b B0 bytes
+                , bytes
+                )
             else if b >= 0xA0 && b <= 0xBF then
                 -- fixstr
-                Nothing
+                ( Err MsgPack.NotImplemented, bytes )
             else if b >= 0xE0 && b <= 0xFF then
                 -- neg fixint
-                Nothing
+                ( Err MsgPack.NotImplemented, bytes )
             else
-                Nothing
+                ( Err MsgPack.NotImplemented, bytes )
