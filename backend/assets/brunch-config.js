35be4cd410e93152525c1c3861af2831f84c942e
Oliver Sõro <oliver.s6ro@gmail.com>
2017-11-23 23:26:23 +0200

    Add elm-mspack package's src/ to brunch's watch list.

--- backend/assets/brunch-config.js
+++ backend/assets/brunch-config.js
@@ -37,7 +37,7 @@ exports.config = {
   // Phoenix paths configuration
   paths: {
     // Dependencies and current project directories to watch
-    watched: ["static", "css", "js", "vendor", "../../frontend"],
+    watched: ["static", "css", "js", "vendor", "../../frontend", "../../src"],
     // Where to compile files to
     public: "../priv/static"
   },
