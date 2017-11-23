9d3961cffe82e62803e030f99d5844ae37527785
Oliver Sõro <oliver.s6ro@gmail.com>
2017-11-23 23:37:01 +0200

    Fix backend' response header and test some signed integers.

--- backend/lib/backend_web/controllers/msgpack_base_types_controller.ex
+++ backend/lib/backend_web/controllers/msgpack_base_types_controller.ex
@@ -3,8 +3,8 @@ defmodule BackendWeb.MsgpackBaseTypesController do
   require Msgpax
 
   def index(conn, _params) do
-    {:ok, packed} = Msgpax.pack(%{name: "John Doe"})
-    Plug.Conn.put_resp_header(conn, "content-type", "text/plain; charset=iso-8859-1")
+    {:ok, packed} = Msgpax.pack([2048, 80910, -39, -4096, -72150]) # 3.14159265358, -2.71828182845, "beer", %{foo: "bar"}])
+    Plug.Conn.put_resp_header(conn, "content-type", "text/plain; charset=x-user-defined")
         |> Plug.Conn.resp(200, IO.iodata_to_binary(packed))
   end
 end
