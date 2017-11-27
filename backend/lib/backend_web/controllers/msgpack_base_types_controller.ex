31c967b4763f6a91ebf8698a683d577b46f43985
Oliver Sõro <oliver.s6ro@gmail.com>
2017-11-27 17:14:26 +0200

    Backend, frontend update to show debug of latest MsgPack state.

--- backend/lib/backend_web/controllers/msgpack_base_types_controller.ex
+++ backend/lib/backend_web/controllers/msgpack_base_types_controller.ex
@@ -3,7 +3,7 @@ defmodule BackendWeb.MsgpackBaseTypesController do
   require Msgpax
 
   def index(conn, _params) do
-    {:ok, packed} = Msgpax.pack([2048, 80910, -39, -4096, -72150]) # 3.14159265358, -2.71828182845, "beer", %{foo: "bar"}])
+    {:ok, packed} = Msgpax.pack(%{foo: "bar", names: %{john: 32, jane: 33}, nums: [-5, 12, -2038, 4096, 100200, -72150], pi: 3.14})
     Plug.Conn.put_resp_header(conn, "content-type", "text/plain; charset=x-user-defined")
         |> Plug.Conn.resp(200, IO.iodata_to_binary(packed))
   end
--- frontend/Main.elm
+++ frontend/Main.elm
@@ -4,6 +4,7 @@ import Hex
 import Html exposing (Html, div, text)
 import Http
 import HttpBuilder exposing (..)
+import Json.Encode
 import Meld exposing (Meld)
 import MsgPack as MP
 import Task exposing (Task)
@@ -103,6 +104,17 @@ msgpackBaseTypes meld =
 
                     l2 =
                         Debug.log "MsgPack Result" msgpack
+
+                    value =
+                        case msgpack of
+                            Err _ ->
+                                Json.Encode.null
+
+                            Ok mp ->
+                                MP.toJson mp |> Result.withDefault Json.Encode.null
+
+                    l3 =
+                        Debug.log "toJson" (Json.Encode.encode 0 value)
                 in
                 meld
             )
